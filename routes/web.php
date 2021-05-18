f052fecfda04fd7b69b6550af9c9cc34f6a7e8a1
Prakhar Gyawali <38656630+PBGyawali@users.noreply.github.com>
2021-05-18 23:08:53 +0200

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\NewsPostController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\AdvertisementController;
+use App\Http\Controllers\VideoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,49 +18,66 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/news/id=')->name('shownews');
+
 Route::get('/category/showall=')->name('showcategory'); //shows edit post form
-Route::get('/',[\App\Http\Controllers\NewsPostController::class, 'all'])->name('allnews');
-Route::get('/news/id={newsPost}', [\App\Http\Controllers\NewsPostController::class, 'show']);
-Route::get('/category/showall={category_id}', [\App\Http\Controllers\NewsPostController::class, 'category']); //shows edit post form   
+Route::get('/',[NewsPostController::class, 'all'])->name('allnews');
+Route::get('/news/id={newsPost}', [NewsPostController::class, 'show']);
+Route::get('/search/', [NewsPostController::class, 'search'])->name('search');
+Route::get('/career', function () {         return view('carreer');})->name('career');
+Route::get('/ad', function () {             return view('ad');})->name('ad');
+Route::get('/aboutus', function () {        return view('aboutus');})->name('about');
+Route::get('/contactus', function () {      return view('contacts');})->name('contacts');
+Route::get('/support', function () {        return view('support');})->name('support');
+Route::get('/privacy', function () {        return view('privacy');})->name('privacy');
+
+Route::get('/category/showall={category_id}', [NewsPostController::class, 'category']); //shows edit post form
 Route::middleware(['auth:sanctum', 'verified'])->group(function(){
-    
-    Route::get('/dashboard',[App\Http\Controllers\NewsPostController::class, 'index'])->name('dashboard');
-    Route::get('/user',[App\Http\Controllers\UserController::class, 'index'])->name('user');
-    Route::get('/user/id={user}', [\App\Http\Controllers\UserController::class, 'edit']); //shows edit post form   
-    Route::get('/user/add', [\App\Http\Controllers\UserController::class, 'create'])->name('adduser');
-    Route::post('/user/add', [\App\Http\Controllers\UserController::class, 'store'])->name('storeuser'); //saves the created post to the databse
-    Route::delete('/user/{user}', [\App\Http\Controllers\UserController::class, 'destroy']); //deletes post from the database
-    Route::put('/user/id={user}', [\App\Http\Controllers\UserController::class, 'update']); //commits edited post to the database 
+
+    Route::get('/dashboard',[NewsPostController::class, 'index'])->name('dashboard');
+    Route::get('/user',[UserController::class, 'index'])->name('user');
+    Route::get('/user/id={user}', [UserController::class, 'edit']); //shows edit post form
+    Route::get('/user/add', [UserController::class, 'create'])->name('adduser');
+    Route::post('/user/add', [UserController::class, 'store'])->name('storeuser'); //saves the created post to the databse
+    Route::delete('/user/{user}', [UserController::class, 'destroy']); //deletes post from the database
+    Route::put('/user/id={user}', [UserController::class, 'update']); //commits edited post to the database
     // The route we have created to show all news posts.
-    Route::get('/news', [\App\Http\Controllers\NewsPostController::class, 'index'])->name('news');
-    
-    Route::get('/news/create', [\App\Http\Controllers\NewsPostController::class, 'create'])->name('createnews'); //shows create post form
-    Route::post('/news/create', [\App\Http\Controllers\NewsPostController::class, 'store'])->name('storenews'); //saves the created post to the databse
-    Route::get('/news/{newsPost}/edit', [\App\Http\Controllers\NewsPostController::class, 'edit']); //shows edit post form
-    Route::put('/news/{newsPost}/edit', [\App\Http\Controllers\NewsPostController::class, 'update']); //commits edited post to the database 
-    Route::delete('/news/{newsPost}', [\App\Http\Controllers\NewsPostController::class, 'destroy']); //deletes post from the database
+    Route::get('/news', [NewsPostController::class, 'index'])->name('news');
+
+    Route::get('/news/create', [NewsPostController::class, 'create'])->name('createnews'); //shows create post form
+    Route::post('/news/create', [NewsPostController::class, 'store'])->name('storenews'); //saves the created post to the databse
+    Route::get('/news/{newsPost}/edit', [NewsPostController::class, 'edit']); //shows edit post form
+    Route::put('/news/{newsPost}/edit', [NewsPostController::class, 'update']); //commits edited post to the database
+    Route::delete('/news/{newsPost}', [NewsPostController::class, 'destroy']); //deletes post from the database
 
 
-       
-    Route::get('/category',[App\Http\Controllers\CategoryController::class, 'index'])->name('category');
-    Route::get('/category/id={category}', [\App\Http\Controllers\CategoryController::class, 'edit']); //shows edit post form   
-    Route::get('/category/add', [\App\Http\Controllers\CategoryController::class, 'create'])->name('addcategory');
-    Route::post('/category/add', [\App\Http\Controllers\CategoryController::class, 'store'])->name('storecategory'); //saves the created post to the databse
-    Route::delete('/category/{category}', [\App\Http\Controllers\CategoryController::class, 'destroy']); //deletes post from the database
-    Route::put('/category/id={category}', [\App\Http\Controllers\CategoryController::class, 'update']); //commits edited post to the database 
 
-    Route::get('/gallery',[App\Http\Controllers\GalleryController::class, 'index'])->name('gallery');
-    Route::get('/gallery/id={gallery}', [\App\Http\Controllers\GalleryController::class, 'edit']); //shows edit post form   
-    Route::get('/gallery/add', [\App\Http\Controllers\GalleryController::class, 'create'])->name('addgallery');
-    Route::post('/gallery/add', [\App\Http\Controllers\GalleryController::class, 'store'])->name('storegallery'); //saves the created post to the databse
-    Route::delete('/gallery/{gallery}', [\App\Http\Controllers\GalleryController::class, 'destroy']); //deletes post from the database
-    Route::put('/gallery/id={gallery}', [\App\Http\Controllers\GalleryController::class, 'update']); //commits edited post to the datgalle
+    Route::get('/category',[CategoryController::class, 'index'])->name('category');
+    Route::get('/category/id={category}', [CategoryController::class, 'edit']); //shows edit post form
+    Route::get('/category/add', [CategoryController::class, 'create'])->name('addcategory');
+    Route::post('/category/add', [CategoryController::class, 'store'])->name('storecategory'); //saves the created post to the databse
+    Route::delete('/category/{category}', [CategoryController::class, 'destroy']); //deletes post from the database
+    Route::put('/category/id={category}', [CategoryController::class, 'update']); //commits edited post to the database
+
+    Route::get('/gallery',[GalleryController::class, 'index'])->name('gallery');
+    Route::get('/gallery/id={gallery}', [GalleryController::class, 'edit']); //shows edit post form
+    Route::get('/gallery/add', [GalleryController::class, 'create'])->name('addgallery');
+    Route::post('/gallery/add', [GalleryController::class, 'store'])->name('storegallery'); //saves the created post to the databse
+    Route::delete('/gallery/{gallery}', [GalleryController::class, 'destroy']); //deletes post from the database
+    Route::put('/gallery/id={gallery}', [GalleryController::class, 'update']); //commits edited post to the datgalle
 
 
-    Route::get('/advertisement',[App\Http\Controllers\AdvertisementController::class, 'index'])->name('advertisement');
-    Route::get('/advertisement/id={advertisement}', [\App\Http\Controllers\AdvertisementController::class, 'edit']); //shows edit post form   
-    Route::get('/advertisement/add', [\App\Http\Controllers\AdvertisementController::class, 'create'])->name('addadvertisement');
-    Route::post('/advertisement/add', [\App\Http\Controllers\AdvertisementController::class, 'store'])->name('storeadvertisement'); //saves the created post to the databse
-    Route::delete('/advertisement/{advertisement}', [\App\Http\Controllers\AdvertisementController::class, 'destroy']); //deletes post from the database
-    Route::put('/advertisement/id={advertisement}', [\App\Http\Controllers\AdvertisementController::class, 'update']); //commits edited post to the datgalle
+    Route::get('/advertisement',[AdvertisementController::class, 'index'])->name('advertisement');
+    Route::get('/advertisement/id={advertisement}', [AdvertisementController::class, 'edit']); //shows edit post form
+    Route::get('/advertisement/add', [AdvertisementController::class, 'create'])->name('addadvertisement');
+    Route::post('/advertisement/add', [AdvertisementController::class, 'store'])->name('storeadvertisement'); //saves the created post to the databse
+    Route::delete('/advertisement/{advertisement}', [AdvertisementController::class, 'destroy']); //deletes post from the database
+    Route::put('/advertisement/id={advertisement}', [AdvertisementController::class, 'update']); //commits edited post to the datgalle
+
+
+    Route::get('/video',[VideoController::class, 'index'])->name('video');
+    Route::get('/video/id={video}', [VideoController::class, 'edit']); //shows edit post form
+    Route::get('/video/add', [VideoController::class, 'create'])->name('addvideo');
+    Route::post('/video/add', [VideoController::class, 'store'])->name('storevideo'); //saves the created post to the databse
+    Route::delete('/video/{video}', [VideoController::class, 'destroy']); //deletes post from the database
+    Route::put('/video/id={video}', [VideoController::class, 'update']); //commits edited post to the datgalle
 });
